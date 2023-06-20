3353491d71f80eccd231dd0b3e248a1727cff621
Sahana Muralikrishnan <hola.sahana@gmail.com>
2023-06-20 17:08:55 +0400

    AOC 2022 Day 12 pt1

---
+++ Day_12/day12.py
@@ -0,0 +1,80 @@
+#AOC 2022 - Day 12
+
+#Dijkstra to find lowest possible path
+
+#Part 1 Answer : 490
+import os
+import pprint
+from collections import deque
+
+moves_arr = []
+todo = deque()
+
+def find_coords(grid, val):
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == val:
+                
+                return (i,j)         
+
+def get_neighbours(x,y):
+    neighbours = []
+    for (i,j) in [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]:
+    
+        if 0<=i<rows and 0<=j<columns and (ord(grid[i][j]) - ord(grid[x][y])) <= 1:
+            neighbours.append((i,j))
+
+    return neighbours
+
+
+def get_shortest_path():
+    visited = set()
+
+    while todo:
+        x,y, moves= todo.popleft()
+
+        if (x,y) in visited:
+            continue
+
+        visited.add((x,y))
+
+        if (x,y) == (endx,endy):
+            return moves
+        
+        for (i,j) in get_neighbours(x,y):
+            if ((i,j, moves+1)) not in todo:
+                todo.append((i,j, moves+1))
+
+def move_next(x,y,moves):
+
+    if grid[x][y] == 'E':
+        moves_arr.append(moves)
+        return
+  
+    for (i,j) in get_neighbours(x,y):     
+        move_next(i,j,moves+1)            
+        
+
+file_path = os.path.join(os.path.dirname('__file__'), "day12.in")
+
+with open(file_path) as file:
+
+    grid = [list(line.strip()) for line in file]
+
+
+
+rows = len(grid)
+columns = len(grid[0])
+(startx,starty) = find_coords(grid,'S')
+(endx,endy) = find_coords(grid, 'E')
+grid[startx][starty] = 'a'
+grid[endx][endy] = 'z'
+
+todo.append((startx, starty, 0))
+
+print(get_shortest_path())
+
+
+
+
+
